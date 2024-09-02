82d4b7ccbb17ce8eb70f32e5a65fd4d63ec11215
yousef aboschwaly <116332558+YousefAboschwaly@users.noreply.github.com>
2024-09-02 18:26:16 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+  "use strict";
+const typed = new Typed(".moveLetter", {
+  strings: ["apps", "websites", "softwares"],
+  loop: true,
+  typeSpeed: 35,
+  backSpeed: 35,
+  backDelay: 1500,
+
+});
